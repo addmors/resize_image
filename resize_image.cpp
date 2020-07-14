25dd12e73dbdeb15a7d4013a2dd9ac3588e0835d
addmors <60542957+addmors@users.noreply.github.com>
2020-07-14 20:28:31 +0300

    Add files via upload

---
+++ resize_image.cpp
@@ -0,0 +1,31 @@
+﻿#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include<windows.h>
+#include <string>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double a =1; 
+	string s= "";
+	cout << "Введите коэффициент маштабирования";
+	cin >> a;
+	cout << "Ведите путь до картинки";
+	cin >> s;
+	
+	Mat image = imread(s, CV_LOAD_IMAGE_COLOR);
+	if (image.empty()) {
+		cout << "Ошибка ввода файла";
+		Sleep(5000);
+		return -1;
+	}
+	cv::resize(image, image, Size(), a, a, 1);
+	imwrite("new_image.jpg", image);
+	imshow("image", image);
+	int key = (waitKey(0) & 0xFF);
+	cout << "Успешно!!!";
+
+}
